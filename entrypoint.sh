7de24a3ebfb7e32a58e3783a37a6949b09454683
minesunny <98507458+minesunny@users.noreply.github.com>
2023-06-18 20:35:02 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 docker_run="docker run"
-docker_run="$docker_run -e POSTGRES_DB=$INPUT_POSTGRESQL_DB"
-docker_run="$docker_run -e POSTGRES_USER=$INPUT_POSTGRESQL_USER"
-docker_run="$docker_run -e POSTGRES_PASSWORD=$INPUT_POSTGRESQL_PASSWORD"
+docker_run="$docker_run -e POSTGRES_DB=postgres"
+docker_run="$docker_run -e POSTGRES_USER=postgres"
+docker_run="$docker_run -e POSTGRES_HOST_AUTH_METHOD=trust"
 docker_run="$docker_run -d -p 5432:5432 postgres:$INPUT_POSTGRESQL_VERSION"
 
 sh -c "$docker_run"
